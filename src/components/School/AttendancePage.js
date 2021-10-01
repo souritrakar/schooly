6164d78ade575e81331255d4942aea35404e7aeb
Aritra Kar <aritrakar2002@gmail.com>
2021-10-01 00:44:30 -0400

    Added attendance functionality.

--- src/components/School/AttendancePage.js
+++ src/components/School/AttendancePage.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebase from "../../firebase";
 
 export default function AttendancePage() {
+  const [attendances, setAttendances] = useState([]);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((cred) => {
+      firebase
+        .firestore()
+        .collection("Users")
+        .doc(cred.email)
+        .collection("Students")
+        .get()
+        .then((doc) => {
+          let temp = [];
+          doc.forEach((d) => {
+            temp.push({ name: d.data().name, date: d.data().attendance });
+          });
+          setAttendances(temp);
+        });
+    });
+  }, []);
+
   return (
     <div>
       <h2> ATTENDANCE PAGE</h2>
